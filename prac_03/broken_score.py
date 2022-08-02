d8e7af37a3de0040619dd714528d373ac70bf8c1
weiyiguo1224 <1471957713@qq.com>
2022-08-02 20:57:53 +0800

    Convert grade in to grade status. and make convert grade as a function.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,20 +1,27 @@
+
 """
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
 
-
-while score < 0 or score > 100:
-    print("invalid input,please input 0-100")
+def main():
+    " gat grade in number and show it in state"
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+    print(grade_status(score))
 
 
+def grade_status(score):
+    " Determine state of giving score "
+    while score < 0 or score > 100:
+        print("invalid input,please input 0-100")
+        score = float(input("Enter score: "))
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
 
+
+main()
