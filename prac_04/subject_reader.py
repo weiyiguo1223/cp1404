e782b8483bb8bdd496a069ae137ee3c10b66b628
weiyiguo1224 <1471957713@qq.com>
2022-08-09 20:12:55 +0800

    get data from subject_data then print it in require format

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,12 +7,14 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """get subject data and print it in format"""
     data = get_data()
-    print(data)
+    display_subject_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +24,15 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts)
         print("----------")
     input_file.close()
+    return data
+
+def display_subject_data (data):
+    """Print subject data in format"""
+    for subject_data in data:
+        print(f"{subject_data[0]} is taught by {subject_data[1]:12} and has {subject_data[2]:3} students")
 
 
 main()
