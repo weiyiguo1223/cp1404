a3a2ee608c739e2a8a75469f91f46656969e411e
weiyiguo1224 <1471957713@qq.com>
2022-08-18 18:55:44 +0800

    delete the print of code name from the program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,7 +7,7 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
