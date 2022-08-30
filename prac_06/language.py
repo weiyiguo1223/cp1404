0ee015ba22cec0971b0a373c2f4c85e1ee73b939
weiyiguo1224 <1471957713@qq.com>
2022-08-30 20:32:34 +0800

    check if typing is dynamic, and print the language with typing is dynamic

--- prac_06/language.py
+++ prac_06/language.py
@@ -3,8 +3,17 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """check dynamical tying language"""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print("The dynamically typed languages are:")
+    languages = [ruby,python,visual_basic]
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+main()
 
 
