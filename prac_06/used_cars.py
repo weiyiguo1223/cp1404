02190b375ae7f5948cf847166cce26fd7f2fb92e
weiyiguo1224 <1471957713@qq.com>
2022-08-30 19:56:07 +0800

    create a new object car Limo

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(fuel=180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,5 +15,14 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    limo = Car("limo",100)
+    limo.add_fuel(20)
+    print(f"Limo fuel = {limo.fuel}")
+    distance_driven = limo.drive(115)
+    print(f"Limo odo = {limo.odometer}")
+    print(f"Actual distance traveled: {distance_driven}")
+    print(limo)
 
 main()
+
+
