a222eb1c51b8a33910176f4a15fdec62e8a15ab8
weiyiguo1224 <1471957713@qq.com>
2022-09-13 20:41:20 +0800

    final code of silver_service_taxi_test.py

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+SilverServiceTaxi class tests
+"""
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
